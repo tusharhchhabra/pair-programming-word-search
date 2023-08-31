a14d371d6c82eebe47d00b5109fb778679be0139
Tushar Chhabra <tusharhchhabra@gmail.com>
2023-08-31 02:06:22 -0700

    Pair programming code

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -20,7 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -34,5 +34,78 @@ describe("#wordSearch()", function() {
     ], 'SEINFELD')
 
     assert.isTrue(result);
+  });
+
+  it("should return true if the word is present vertically", function() {
+    // Starts at row 0, column 4, and down 5 chars
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'S', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'P', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'A', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'R', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'E', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SPARE')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the letter array is empty", function() {
+    const result = wordSearch([], 'SPARE')
+    assert.isFalse(result);
+  });
+
+  it("should return false if no word is provided", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'S', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'P', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'A', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'R', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'E', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ])
+    assert.isFalse(result);
+  });
+
+  it('should return false if each row is not the same length', function() {
+    assert.throws(() => wordSearch([
+      ['A', 'W', 'C', 'F', 'S', 'U', 'A', 'L'],
+      ['Y', 'F', 'C', 'F',],
+      ['S', 'E', 'I', 'N', 'P'],
+    ], 'EXAMPLE'), "All rows must be the same length");
+  });
+
+  it('should return false if there are any numbers in the array', function() {
+   assert.throws(() => wordSearch([
+      [1, 'W', 'C', 'F', 'S', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'P', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'A', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'R', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'E', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'A', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SPARE'), "Please enter a matrix of letters only");
+  });
+
+  it('should return false if there are any numbers as strings in the array', function() {
+   assert.throws(() => wordSearch([
+      ['1', 'W', 'C', 'F', 'S', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'P', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'A', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'R', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'E', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'A', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SPARE'), "Please enter a matrix of letters only");
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,40 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  if (!Array.isArray(letters) || letters.length === 0 || !word) {
+    return false;
+  }
 
-module.exports = wordSearch
+  // Check if input is consistent
+  const rowLength = letters[0].length;
+  letters.forEach(row => {
+    if (row.length !== rowLength) {
+      throw new Error("All rows must be the same length");
+    }
+
+    row.forEach(letter => {
+      if (typeof letter !== 'string') throw new Error("Please enter a matrix of letters only");
+      let alphabet = 'abcdefghijklmnopqrstuvwyz';
+      if (!alphabet.includes(letter.toLowerCase())) throw new Error("Please enter a matrix of letters only");
+    });
+  });
+
+  const horizontalJoin = letters.map(ls => ls.join(''));
+
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  let x = letters[0].length;
+  let y = letters.length;
+  for (let i = 0; i < x; i++) {
+    let column = "";
+    for (let j = 0; j < y; j++) {
+      let letter = letters[j][i];
+      column += letter;
+    }
+    if (column.includes(word)) return true;
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
